2adcf3f7adb4c35f2fbbcf0a327e39bddf4db904
MaheyR <mahey.ralambo@edu.univ-fcomte.fr>
2026-03-11 15:25:05 +0100

    Push SAE-203 URL

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090')
+const pb = new PocketBase('https://sae-203-savoufest.maheyralambo.fun:443')
 
 export async function getAllArtistesSortedByDate() {
   const representations = await pb.collection('representations').getFullList({
--- backend/debug-pb.js
+++ backend/debug-pb.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090')
+const pb = new PocketBase('https://sae-203-savoufest.maheyralambo.fun:443')
 
 async function debug() {
   try {
